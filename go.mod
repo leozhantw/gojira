f190ea37349390947960353060c56c27dfb38abd
LeoZhan <leo3838ok@gmail.com>
2020-11-22 23:02:26 +0800

    feat: initial project structure

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/leozhantw/gojira
+
+go 1.14
+
+require github.com/spf13/cobra v1.1.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/leozhantw/gojira/cmd"
+
+func main() {
+	cmd.Execute()
+}
